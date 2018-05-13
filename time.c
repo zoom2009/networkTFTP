9408aed8571d5d82687d36d0e23b54f6422eb92a
unknown <afumagic3@gmail.com>
2018-05-13 02:35:54 +0100

    make fn

--- time.c
+++ time.c
@@ -5,22 +5,19 @@
 static void wakeup(int id){
 	printf("WakeUp\n");
 }
-static void wakeup2(int id){
-        printf("WakeUp2\n");
-}
 
-main(){
+void timeW8(){
 	int p;
-	char buf[100];
-	struct sigaction act;
-	act.sa_handler=wakeup;
-	sigemptyset(&act.sa_mask);
-	act.sa_flags=0;
-	sigaction(SIGALRM,&act,0);
+        char buf[100];
+        struct sigaction act;
+        act.sa_handler=wakeup;
+        sigemptyset(&act.sa_mask);
+        act.sa_flags=0;
+        sigaction(SIGALRM,&act,0);
 
-	alarm(3);
+        alarm(3);
 
-	if((p=read(STDIN_FILENO,buf,100))<0){
+        if((p=read(STDIN_FILENO,buf,100))<0){
                 if (errno==EINTR){
                         printf("Interupt System Call\n");
                 }else{
@@ -29,28 +26,10 @@ main(){
         }else{
                 printf("System Call ok\n");
         }
+}
 
-
-	struct sigaction act2;
-	act2.sa_handler=wakeup2;
-        sigemptyset(&act2.sa_mask);
-        act2.sa_flags=0;
-        sigaction(SIGALRM,&act2,0);
-
-	alarm(3);
-	//Point of problem//
-	//if((p=pause())<0){
-	///*
-	if((p=read(STDIN_FILENO,buf,100))<0){
-		if (errno==EINTR){
-			printf("Interupt System Call\n");
-		}else{
-			printf("General System Call Error\n");
-		}
-	}else{
-		printf("System Call ok\n");
-	}
-	//*/
-
+main(){
+	timeW8();
+	timeW8();
 }
 
