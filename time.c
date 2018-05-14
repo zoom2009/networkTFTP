18a19a55dae92a86fa8c7242ce746d7c7dd649e0
unknown <afumagic3@gmail.com>
2018-05-14 01:20:23 +0100

    timeout done

--- time.c
+++ time.c
@@ -2,34 +2,46 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <errno.h>
-static void wakeup(int id){
-	printf("WakeUp\n");
+static void timeOut(int id){
+	printf("\n!!!!!!!!!!!!wakeUp!!!!!!!!!!!!!!\n");
 }
+static void goNext(int id){
+        printf("\n!!!!!!!!!!!!Gonext!!!!!!!!!!!!!!\n");
+}
+
+
 
 void timeW8(){
 	int p;
         char buf[100];
         struct sigaction act;
-        act.sa_handler=wakeup;
+        act.sa_handler=timeOut;
         sigemptyset(&act.sa_mask);
         act.sa_flags=0;
         sigaction(SIGALRM,&act,0);
-
-        alarm(3);
-
-        if((p=read(STDIN_FILENO,buf,100))<0){
-                if (errno==EINTR){
-                        printf("Interupt System Call\n");
-                }else{
-                        printf("General System Call Error\n");
-                }
-        }else{
-                printf("System Call ok\n");
-        }
 }
+
+void timeW82(){
+        int p;
+        char buf[100];
+        struct sigaction act;
+        act.sa_handler=goNext;
+        sigemptyset(&act.sa_mask);
+        act.sa_flags=0;
+        sigaction(SIGALRM,&act,0);
+}
+
 
 main(){
 	timeW8();
-	timeW8();
+	alarm(5);
+	timeW82();
+	alarm(1);
+	//alarm(0);
+	printf("sss");
+	//timeW8();
+	while(1){
+		//printf("1\n");
+	}
+	printf("done");
 }
-
